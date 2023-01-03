1a2e5694a00efef871d46c50e7f936afbc69f873
erfascio <alessandrofascini@proton.me>
2023-01-03 22:26:39 +0100

    adding fake users for testing

--- src/test/java/it/eforhum/users/UsersApplicationTests.java
+++ src/test/java/it/eforhum/users/UsersApplicationTests.java
@@ -1,10 +1,45 @@
 package it.eforhum.users;
 
+import it.eforhum.users.entity.User;
+import it.eforhum.users.service.UserService;
+import it.eforhum.users.utils.DateGenerator;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.*;
+import java.time.temporal.TemporalAccessor;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 class UsersApplicationTests {
+	private static Random randomGenerator = new Random();
+	private static List<User> users = new ArrayList<>();
+
+	static  {
+		System.out.println("Pre Load elements");
+		users.add(new User());
+		users.get(0).setUsername("root");
+		users.get(0).setPassword("root");
+		users.get(0).setBornDate(DateGenerator.defaultLocalDate());
+		users.get(0).setLastLoginDate(DateGenerator.defaultZonedDateTime());
+		users.get(0).setRoles("ROLES_ROOT");
+
+		IntStream.range(1, 5).forEach(userId -> {
+			User user = new User();
+			user.setUsername("user#" + userId);
+			user.setPassword("user:pwd#" + userId);
+			user.setBornDate(DateGenerator.defaultLocalDate());
+			user.setLastLoginDate(DateGenerator.defaultZonedDateTime());
+			user.setRoles("ROLES_SIMPLE");
+			users.add(user);
+		});
+	}
 
 	@Test
 	void contextLoads() {
--- src/test/java/it/eforhum/users/utils/DateGenerator.java
+++ src/test/java/it/eforhum/users/utils/DateGenerator.java
@@ -1,2 +1,37 @@
-package it.eforhum.users.utils;public class DateGenerator {
+package it.eforhum.users.utils;
+
+import java.time.*;
+import java.util.Random;
+
+public class DateGenerator {
+    private DateGenerator() {
+        // Do Nothing
+    }
+    private static Random randomGenerator = new Random();
+
+    private static long MAX_EPOCH_DAY = 1672527600000L;
+    private static long MIN_EPOCH_DAY = 0L;
+    public static Long randomEpochDay() {
+        return (long) (Math.random() * (MAX_EPOCH_DAY - MIN_EPOCH_DAY)) + MIN_EPOCH_DAY;
+    }
+    public static Long randomEpochDay(long max_epoch_day) {
+        long epoch_day = Math.abs(max_epoch_day);
+        return (long) (Math.random() * (epoch_day - MIN_EPOCH_DAY + 1)) + MIN_EPOCH_DAY;
+    }
+
+    public static LocalDate randomLocalDate() {
+        return LocalDate.now();
+    }
+
+    public static LocalDate randomLocalDate(LocalDate min, LocalDate max) {
+        return randomLocalDate();
+    }
+
+    public static LocalDate defaultLocalDate() {
+        return LocalDate.of(1970, 1, 1);
+    }
+
+    public static ZonedDateTime defaultZonedDateTime() {
+        return ZonedDateTime.of(DateGenerator.randomLocalDate(), LocalTime.of(0, 0, 0), ZoneId.systemDefault());
+    }
 }
