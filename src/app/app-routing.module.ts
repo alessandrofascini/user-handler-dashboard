a6583a22f8539ad1eb20885108a72b81b5be306c
Alessandro Fascini <alessandrofascini@proton.me>
2023-01-04 19:12:49 +0100

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'users',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'list',
+      },
+      {
+        path: 'details/:id',
+      },
+      {
+        path: 'update/:id',
+      }
+    ],
+    redirectTo: '/dashboard/user-list/'
+    // canActivate[AuthFilter]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
