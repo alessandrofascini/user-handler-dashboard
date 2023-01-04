cf6fc1445972bcc39e98d07177a89e281cc5ab33
Alessandro Fascini <alessandrofascini@proton.me>
2023-01-04 19:45:26 +0100

    navigation settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {UserListComponent} from "./user-list/user-list.component";
+import {UserDetailsComponent} from "./user-details/user-details.component";
+import {UserUpdateComponent} from "./user-update/user-update.component";
 
 const routes: Routes = [
   {
@@ -19,15 +22,17 @@ const routes: Routes = [
     children: [
       {
         path: 'list',
+        component: UserListComponent,
       },
       {
         path: 'details/:id',
+        component: UserDetailsComponent,
       },
       {
         path: 'update/:id',
+        component: UserUpdateComponent,
       }
     ],
-    redirectTo: '/dashboard/user-list/'
     // canActivate[AuthFilter]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserUpdateComponent } from './user-update/user-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    UserListComponent,
+    UserDetailsComponent,
+    UserUpdateComponent
   ],
   imports: [
     BrowserModule,
